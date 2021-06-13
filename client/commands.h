a581bdea1a7ff9ae0f65eaa4bdcb86055d9ba0a4
Yaroslav Pristalov <pristalovya@yandex.ru>
2021-06-13 23:17:12 +0300

    Passwords and requests added

---
+++ client/commands.h
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdio.h>
+#include <string.h>
+int IsCommand(char *str);
+void CommandDefAndRegUp(char* str, char* command);
+void HelpCommand();
+int IsCommandCorrect(char* str, int numberOfGrills);
+void SendRequest(int socketFileDescriptor, char* textOfRequest, char *message);
+int CommandAnalyzer(char* name, char* message,int socketFileDescriptor);
+#endif // COMMANDS_H
--- client/utils.h
+++ client/utils.h
@@ -7,6 +7,11 @@ typedef char Bool;
 #define TRUE 1
 #define FALSE 0
 
+#define MES_LENGTH 2048
+#define DELTA 32
+#define NAME_LENGTH 64
+#define COMMAND_LEN 32
+
 extern volatile sig_atomic_t run;
 
 void catch_ctrl_c_and_exit(int signal);
